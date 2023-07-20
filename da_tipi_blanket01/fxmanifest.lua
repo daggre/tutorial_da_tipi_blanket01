2ffe96fdc728278dbadef4534ba799740d46b7cb
daggre_actual <dgract@gmail.com>
2023-07-20 00:03:34 -0700

    Final files for da_tipi_blanket01 tutorial

---
+++ da_tipi_blanket01/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'adamant'
+games { 'rdr3' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+this_is_a_map 'yes'
+lua54 'yes'
+
+files { 'stream/*.ytyp' }
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
